c136b2d243fa2ca60ec25ef1908559f9e3361d6c
dagapurva <dagapurva3@gmail.com>
2025-01-25 13:27:56 -0500

    test1

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('./banner-bg.png')",
+        "hero-pattern": "url('/banner-bg.png')",
       },
     },
   },
