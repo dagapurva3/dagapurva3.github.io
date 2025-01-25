d4bec2e920cf589b141f03ad0758ebc0e7ccf03c
dagapurva <dagapurva3@gmail.com>
2025-01-25 12:25:48 -0500

    Deploy portfolio to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    output: 'export',
+    assetPrefix: isProd ? '/dagapurva3.github.io/' : '',
+    images: {
+        unoptimized: true,
+    },
     env: {
-        TEMPLATE_ID : process.env.TEMPLATE_ID,
-        SERVICE_ID : process.env.SERVICE_ID,
-        PUBLIC_KEY : process.env.PUBLIC_KEY,
+        TEMPLATE_ID: process.env.TEMPLATE_ID,
+        SERVICE_ID: process.env.SERVICE_ID,
+        PUBLIC_KEY: process.env.PUBLIC_KEY,
     },
 }
 
